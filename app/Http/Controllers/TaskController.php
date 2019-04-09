70544dea73cdc758dc45054f3ba10ee3889d6bf3
kter <me@kter.jp>
2019-04-09 23:36:35 +0900

    middlewareを追加

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,5 +8,13 @@ use App\Http\Requests;
 
 class TaskController extends Controller
 {
-    //
+    /**
+     * 新しいコントローラインスタンスの生成
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 }
