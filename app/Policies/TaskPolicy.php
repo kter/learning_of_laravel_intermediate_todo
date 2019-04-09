2754ce36eb7df916474c2c0b9f1ca1afa2089a25
kter <me@kter.jp>
2019-04-10 01:10:46 +0900

    ポリシーの設定

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -2,6 +2,8 @@
 
 namespace App\Policies;
 
+use App\User;
+use App\Task;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class TaskPolicy
@@ -16,5 +18,17 @@ class TaskPolicy
     public function __construct()
     {
         //
+    }
+
+    /**
+     * 指定されたユーザーが指定されたタスクを削除できるか決定
+     *
+     * @param User $user
+     * @param Task $task
+     * @return bool
+     */
+    public function destroy(User $user, Task $task)
+    {
+        return $user->id === $task->user_id;
     }
 }
